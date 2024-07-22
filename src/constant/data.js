00fa2b54b3ada2ae51e81e4068660ed8cbe92fa7
Silferion <dizacn.prsn@gmail.com>
2024-07-22 13:13:32 +0800

    first commit

--- src/constant/data.js
+++ src/constant/data.js
@@ -1,5 +1,5 @@
-// export const apiEndPoint = 'https://mab-solutions.online';
-export const apiEndPoint = 'http://127.0.0.1:8000';
+export const apiEndPoint = 'https://mab-solutions.online';
+// export const apiEndPoint = 'http://127.0.0.1:8000';
 
 export const listPrTableData = [
 		{
